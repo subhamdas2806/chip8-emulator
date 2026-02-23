f07993e8d81a0010fbb9812a4bf397b26e69c9c2
Subham <dassubham7756@gmail.com>
2026-02-23 23:57:19 +0530

    daily commit

---
+++ platform.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <cstdint>
+
+class Platform
+{
+    public:
+        Platform(char const* title , int windowWidth , int windowHeight, int textureWidth , int textureHeight);
+        ~Platform();
+        void Update(void const* buffer, int pitch);
+        bool ProcessInput(uint8_t* keys);
+    private:
+        SDL_Window* window{};
+        SDL_Renderer* renderer{};
+        SDL_Texture* texture{};
+};
